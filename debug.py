035c98731196ef550fb5ee43dfbf015c4dd0a869
dujuguo <juguodu@163.com>
2024-08-24 13:28:53 +0800

    first submit

---
+++ debug.py
@@ -0,0 +1,14 @@
+s = "ADOBECODEBANC"
+t = "ABC"
+
+cur_length, max_length = float("inf"), float("inf")
+result = ""
+l = 0
+for r in range(len(s)):
+    while t in s[l:r]:
+        cur_length = min(cur_length, r-l+1)
+        l += 1
+    if cur_length < max_length:
+        result = s[l-1:r]
+        
+result
